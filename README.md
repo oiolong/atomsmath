248640b0e683ace0d3c035b2a11d8f61cfdb6198
allen <oiolong@gmail.com>
2021-02-01 23:43:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# atomsmath
+
+> my son is on winter vacation, he is not good at math , so i make a game project for him to practice math
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+<div class="portfolioContainer">
+<div v-for="(mm , index) in m" class="ImageCol">
+<div class="portfolioContent">
+<h3> {{ index }}. {{ mm.dividends }} ÷ {{ mm.divisors }} =</h3>
+<p> 
+</p>
+</div>
+</div>
+</div>
+
+</div>
+</template>
+
+<script>
+const ms = [];
+for (var i = 0; i < 20; i++){
+  var dividend =  Math.floor((Math.random() * 999) + 1);
+  var divisor = Math.floor((Math.random() * 299) + 1);
+  ms.push({dividends : dividend,
+    divisors : divisor,
+    quotients : Math.floor(dividend / divisor),
+    remainders : dividend % divisor 
+  });
+}
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg:"division practice for grades 3 at TianMu elementary school!",
+      m:ms
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+* {
+box-sizing: border-box;
+}
+
+h1 {
+font-size: 30px;
+text-align: center;
+font-family: monospace, serif, sans-serif;
+}
+
+body {
+background-color: #f1f1f1;
+padding: 5px;
+font-family: Arial;
+}
+main {
+max-width: 1000px;
+margin: auto;
+}
+.portfolioContainer {
+margin: 8px -16px;
+}
+.portfolioContainer,
+.portfolioContainer > .ImageCol {
+padding: 0px;
+}
+.ImageCol {
+float: left;
+width: 25%;
+height: 165px;
+}
+.portfolioContainer:after {
+content: "";
+display: table;
+clear: both;
+}
+.portfolioContent {
+float: left;
+padding: 10px;
+}
+@media screen and (max-width: 900px) {
+.ImageCol {
+width: 50%;
+}
+}
+@media screen and (max-width: 600px) {
+.ImageCol {
+width: 100%;
+}
+}
+
+
+</style>
